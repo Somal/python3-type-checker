d57979d125c05722de382c2ce1c84fb3e09edc4c
somal <somal1996@rambler.ru>
2016-08-21 16:08:01 +0300

    Add install file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='python3-type-checker',
+        version='1.0',
+        packages=[''],
+        url='https://github.com/Somal/python3-type-checker',
+        license='MIT',
+        author='Sokolov Maxim',
+        author_email='m.sokolov@email.ru',
+        description='Type checker for Python 3'
+)
